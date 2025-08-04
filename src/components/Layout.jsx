e49ab9545534cf911e8f0c34203fee409a1ca90c
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-08-04 23:40:28 +0530

    add whatsapp button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 // src/components/Layout.jsx
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import WhatsAppFloatingButton from "./WhatsAppFloatingButton";
 
 export default function Layout() {
     return (
@@ -9,6 +10,7 @@ export default function Layout() {
             <main className="p-[1px]">
                 <Outlet />
             </main>
+            <WhatsAppFloatingButton />
         </div>
     );
 }
---
+++ src/components/WhatsAppFloatingButton.jsx
@@ -0,0 +1,17 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function WhatsAppFloatingButton() {
+    const phoneNumber = "9348727159"; // replace with your actual number
+    const message = "Hey there";
+
+    return (
+        <a
+            href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fixed bottom-5 right-5 z-50 bg-white text-white p-1 rounded-full shadow-lg transition-all duration-300"
+        >
+            <FaWhatsapp className="text-orange-400 md:size-8 size-6 " />
+        </a>
+    );
+}
