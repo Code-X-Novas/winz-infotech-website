d6f1ec46e75e011f4d1d4191e30b1d0988eb52a4
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-12-16 17:36:57 +0530

    ad htaccess

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      }
+    }
+  }
 })
